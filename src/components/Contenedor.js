03dacc9f69d1d980451593fe6f01c0d386623eea
Tomasmallo22 <tomasmallolaino@gmail.com>
2021-09-14 20:57:00 -0300

    Agregando proyecto

---
+++ src/components/Contenedor.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+const Contenedor = styled.div`
+  display:flex;
+  align-items:center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+export default Contenedor
